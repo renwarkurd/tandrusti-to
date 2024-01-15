566ab60123f42fe71414dddbf13094bbe72147e2
Renwar <renwarkurd@gmail.com>
2024-01-15 11:24:18 +0300

    add routes

---
+++ app/Http/Middleware/GetRequestTokenAuthenticateClient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class GetRequestTokenAuthenticateClient
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $token = PersonalAccessToken::findToken($request->token)->can('is-client');
+
+        if ($token) {
+            return $next($request);
+        }
+
+        return response()->json(['message' => 'You are unauthorized'], 422);
+    }
+}
