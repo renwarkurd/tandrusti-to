d3432b869a263f33dc5a8e101a52f98a877c0850
humaidaAra <hama3662@gmail.com>
2024-01-15 16:00:19 +0300

    Database Seeder Dummy Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\City;
+use App\Models\Patient;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +17,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'muhammad',
+            'email' => 'muhammad@mail.com',
+            'username' => 'muhammad',
+            'password' => 'muhammad',
+        ]);
+
+        City::factory()->count(3)->create();
+        Patient::factory()->count(30)->create();
     }
 }
