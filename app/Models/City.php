b1a867a63c78d97462f29b8192ed3b672f8e490b
Renwar <renwarkurd@gmail.com>
2024-01-16 16:09:06 +0300

    updated timestamps in api

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class City extends Model
 {
@@ -14,4 +15,9 @@ class City extends Model
         'user_id',
         'name',
     ];
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
